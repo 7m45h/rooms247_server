676bbcd7f336649359bc8363fc916e421057fbc5
tmash <132013534+7m45h@users.noreply.github.com>
2023-12-23 15:55:37 +0530

    complete

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+import os
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.path.join(os.path.abspath(os.path.dirname(__file__)), 'rooms.db')}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 api = Api(app)
+db = SQLAlchemy(app)
 
-rooms = {
-    "room": [
-        { "id": 123456789, "addr": "a/b/c", "tele": 123456789 },
-        { "id": 987654321, "addr": "c/b/a", "tele": 987654321 }
-    ]
-}
+class RoomModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    addr = db.Column(db.String(512), nullable=False)
+    tele = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"<Room {self.id}>"
 
 room_get_args = reqparse.RequestParser()
 room_get_args.add_argument("id", type=int, help="room id")
@@ -22,28 +28,42 @@ room_post_args.add_argument("tele", type=int, help="telephone number", required=
 room_delete_args = reqparse.RequestParser()
 room_delete_args.add_argument("id", type=int, help="room id", required=True)
 
+rooms_fields = {
+    "id": fields.Integer,
+    "addr": fields.String,
+    "tele": fields.Integer
+}
+
 class Room(Resource):
+    @marshal_with(rooms_fields)
     def get(self):
         args = room_get_args.parse_args()
         if args["id"] == None:
-            return rooms
+            results = RoomModel.query.all()
         else:
-            for room in rooms["room"]:
-                if room["id"] == args["id"]:
-                    return {"room": [room]}
+            results = RoomModel.query.filter_by(id=args["id"]).first()
 
+        return results
+
+    @marshal_with(rooms_fields)
     def post(self):
         args = room_post_args.parse_args()
-        rooms["room"].append(args)
-        return {"room": [args]}, 201
+        room = RoomModel(id=args["id"], addr=args["addr"], tele=args["tele"])
+        db.session.add(room)
+        db.session.commit()
+        return room, 201
 
+    @marshal_with(rooms_fields)
     def delete(self):
         args = room_delete_args.parse_args()
-        for room in rooms["room"]:
-            if room["id"] == args["id"]:
-                rooms["room"].remove(room)
-                return {"room": [room]}
-        
+        room = RoomModel.query.get(args["id"])
+        if room != None:
+            db.session.delete(room)
+            db.session.commit()
+            return room
+
+        return {"error": "not found"}, 404
+            
 
 api.add_resource(Room, "/room")
 
