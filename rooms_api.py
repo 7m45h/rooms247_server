5c1f93b4cffa45f4b8e24a843fa8b465a92e237c
tmash <132013534+7m45h@users.noreply.github.com>
2024-01-19 17:21:08 +0530

    remove del_key

--- rooms_api.py
+++ rooms_api.py
@@ -19,7 +19,6 @@ class RoomModel(db.Model):
     dist = db.Column(db.String(64), nullable=False)
     addr = db.Column(db.String(512), nullable=False)
     tele = db.Column(db.String(64), nullable=False)
-    del_key = db.Column(db.String(64), nullable=False)
 
     def __repr__(self):
         return f"<Room {self.room_id}>"
@@ -29,21 +28,16 @@ def get_rooms():
 
     return results
 
-def add_room(room_id, dist, addr, tele, del_key):
-    room = RoomModel(room_id=room_id, dist=dist, addr=addr, tele=tele, del_key=del_key)
+def add_room(room_id, dist, addr, tele):
+    room = RoomModel(room_id=room_id, dist=dist, addr=addr, tele=tele)
     db.session.add(room)
     db.session.commit()
     return [room]
 
-def del_room(room_id, del_key):
+def del_room(room_id):
     room = RoomModel.query.get(room_id)
     if room == None:
         return None
-
-    if room.del_key == del_key:
-        db.session.delete(room)
-        db.session.commit()
-        return [room]
 
     return None
 
@@ -53,11 +47,9 @@ room_post_args.add_argument("room_id", type=str, help="room room_id required", r
 room_post_args.add_argument("dist", type=str, help="distance required", required=True)
 room_post_args.add_argument("addr", type=str, help="room address required", required=True)
 room_post_args.add_argument("tele", type=str, help="telephone number required", required=True)
-room_post_args.add_argument("del_key", type=str, help="delete key required", required=True)
 
 room_delete_args = reqparse.RequestParser()
 room_delete_args.add_argument("room_id", type=str, help="room room_id required", required=True)
-room_delete_args.add_argument("del_key", type=str, help="delete key required", required=True)
 
 def mk_room_res(rooms):
     room = {"room": []}
@@ -73,13 +65,13 @@ class Room(Resource):
 
     def post(self):
         args = room_post_args.parse_args()
-        room = add_room(args["room_id"], args["dist"], args["addr"], args["tele"], args["del_key"])
+        room = add_room(args["room_id"], args["dist"], args["addr"], args["tele"])
         room = mk_room_res(room)
         return room, 201
 
     def delete(self):
         args = room_delete_args.parse_args()
-        room = del_room(args["room_id"], args["del_key"])
+        room = del_room(args["room_id"])
         if room == None:
             return {"error": "not found"}, 404
 
